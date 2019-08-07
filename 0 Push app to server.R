1f780a8e8f00fcab9e8f904fe5c2499e38e18ea2
kamransattary <kamransattary@gmail.com>
2019-08-07 13:28:16 +0200

    fixed app deployment to include initial.app.path and copy.path at start

--- 0 Push app to server.R
+++ 0 Push app to server.R
@@ -1,34 +1,36 @@
+app.path='/home/rstudio/Dropbox/GTA cloud/0 dev/delta-app-ks/'
+copy.path='/home/rstudio/ShinyApps/tariff/'
 
 ##### DON'T USE THIS UNLESS YOU KNOW WHAT YOU ARE DOING!
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/app.R",
-          "/home/rstudio/ShinyApps/tariff/app.R", overwrite = T)
+file.copy(paste0(app.path,"app.R"),
+          paste0(copy.path,"app.R"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/ui.R",
-          "/home/rstudio/ShinyApps/tariff/ui.R", overwrite = T)
+file.copy(paste0(app.path,"ui.R"),
+          paste0(copy.path,"ui.R"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/server.R",
-          "/home/rstudio/ShinyApps/tariff/server.R", overwrite = T)
+file.copy(paste0(app.path,"server.R"),
+          paste0(copy.path,"server.R"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/functions/table_filter.R",
-          "/home/rstudio/ShinyApps/tariff/functions/table_filter.R", overwrite = T)
+file.copy(paste0(app.path,"functions/table_filter.R"),
+          paste0(copy.path,"functions/table_filter.R"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/functions/xlsx_query.R",
-          "/home/rstudio/ShinyApps/tariff/functions/xlsx_query.R", overwrite = T)
+file.copy(paste0(app.path,"functions/xlsx_query.R"),
+          paste0(copy.path,"functions/xlsx_query.R"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/www/gta logo-white.svg",
-          "/home/rstudio/ShinyApps/tariff/www/gta logo-white.svg", overwrite = T)
+file.copy(paste0(app.path,"www/gta logo-white.svg"),
+          paste0(copy.path,"www/gta logo-white.svg"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/www/loading.svg",
-          "/home/rstudio/ShinyApps/tariff/www/loading.svg", overwrite = T)
+file.copy(paste0(app.path,"www/loading.svg"),
+          paste0(copy.path,"www/loading.svg"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/www/style.css",
-          "/home/rstudio/ShinyApps/tariff/www/style.css", overwrite = T)
+file.copy(paste0(app.path,"www/style.css"),
+          paste0(copy.path,"www/style.css"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/www/tipped.css",
-          "/home/rstudio/ShinyApps/tariff/www/tipped.css", overwrite = T)
+file.copy(paste0(app.path,"www/tipped.css"),
+          paste0(copy.path,"www/tipped.css"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/www/tipped.js",
-          "/home/rstudio/ShinyApps/tariff/www/tipped.js", overwrite = T)
+file.copy(paste0(app.path,"www/tipped.js"),
+          paste0(copy.path,"www/tipped.js"), overwrite = T)
 
-file.copy("/home/rstudio/Dropbox/GTA cloud/17 Shiny/6 Tariff database/app/www/tooltips.js",
-          "/home/rstudio/ShinyApps/tariff/www/tooltips.js", overwrite = T)
+file.copy(paste0(app.path,"www/tooltips.js"),
+                 paste0(copy.path,"www/tooltips.js"), overwrite = T)
