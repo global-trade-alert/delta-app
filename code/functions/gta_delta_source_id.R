453e61e48497d72777cdd465dd17c592de8b9d81
Johannes <fritz.johannes@gmail.com>
2019-08-26 15:21:39 +0200

    small helper function to extract source IDs and create new entries in the source.log

---
+++ code/functions/gta_delta_source_id.R
@@ -0,0 +1,34 @@
+gta_delta_source_id(source.description=NULL,
+                    source.official=NULL,
+                    create.source=F,
+                    db.connection=db.connection){
+  
+  
+  if(is.null(source.description)|is.null(source.official)|is.na(as.logical(source.official))){
+    stop("Please specify a proper source and whether it is official (T/F).")
+  }
+  
+  query=paste("SELECT source_id 
+              FROM delta_source_log 
+              WHERE state_act_source='",
+              source.description,"';")
+  
+  this.source.id=gta_sql_get_value(query)
+  
+  if(is.na(this.source.id) & create.source==T){
+    
+    source.log.update=data.frame(state.act.source=source.description,	
+                                 is.source.official=source.official,
+                                 stringsAsFactors = F)
+    
+    this.source.id=gta_sql_append_table(append.table = "source.log",
+                                        append.by.df = "source.log.update",
+                                        get.id = "source.id")
+    
+    rm(source.log.update)
+    
+  }
+  
+  return(this.source.id)
+}
+
